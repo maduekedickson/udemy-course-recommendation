ec7c8d5c2e376b049eace1cd1818b0b6dfd7413a
Dick Madueke Chukwu <82809958+maduekedickson@users.noreply.github.com>
2024-12-29 11:13:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ df = clean_course_titles(df)
 st.title("Udemy Course Recommendation")
 
 # Display the logo or image
-st.image("image.jpg", use_column_width=True)
+st.image("image.jpg")
 
 # User input for course title
 course_title_input = st.text_input("Enter a course keyword or title", "")
